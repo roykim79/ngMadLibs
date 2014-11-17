84b5c3006be32878cc2082a7ec22da6a1685742a
Roy Kim <rykim79@gmail.com>
2014-11-17 14:22:20 -0800

    created a better user experience

--- js/myApp.js
+++ js/myApp.js
@@ -1,4 +1,21 @@
 angular.module('myApp', [])
 	.controller('MyCtrl', function($scope){
-		
+		$scope.submit = function(){
+			if ($scope.myForm.$valid) {
+				$scope.submitted = true;
+			}
+		};
+		$scope.startOver = function(){
+			$scope.submitted = false;
+			
+			$scope.maleName = "";
+			$scope.dirtyTask = "";
+			$scope.obnoxiousCelebrity = "";
+			$scope.jobTitle = "";
+			$scope.celebrity = "";
+			$scope.hugeNumber = "";
+			$scope.tediousTask = "";
+			$scope.uselessSkill = "";
+			$scope.adjective = "";
+		};
 	});
